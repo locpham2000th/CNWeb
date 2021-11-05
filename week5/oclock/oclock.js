0e43d6847f1d7391c9db505ee988083b34c66d58
Lộc Phạm <locpham2000th@gmail.com>
2021-11-05 22:03:44 +0700

    labai

---
+++ week5/oclock/oclock.js
@@ -0,0 +1,11 @@
+// var canvas = document.getElementById("dongho");
+// var ctx = canvas.getContext("2d");
+// ctx.moveTo(0,0);
+// ctx.lineTo(100,150);
+// ctx.stroke();
+
+var c = document.getElementById("myCanvas");
+        var ctx = c.getContext("2d");
+        ctx.moveTo(0,0);
+        ctx.lineTo(200,100);
+        ctx.stroke();
